c6bb9e8640f0c12091c0868150adb6b9ad764042
Dylan <dylanyoung.dsy@gmail.com>
2022-09-16 11:32:28 +0200

    added alert box to cancel button

--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:pit_pro_app/components/confirm_alert_dialog.dart';
 
 Widget cancelButton(
-    BuildContext context, List<HiveObject> objList, bool confirmed) {
-  if (confirmed) {
+    BuildContext context, List<HiveObject> objList, bool isNotEditing) {
+  if (isNotEditing) {
     return ElevatedButton(
-        style: ElevatedButton.styleFrom(),
-        child: Row(
-          children: const [
-            Text('Cancel '),
-            Icon(Icons.cancel_outlined, size: 20),
-          ],
-        ), //const ,
-        onPressed: () {
-          for (var element in objList) {
-            element.delete();
-          }
-          Navigator.of(context).pop();
-        });
+      style: ElevatedButton.styleFrom(),
+      child: Row(
+        children: const [
+          Text('Cancel '),
+          Icon(Icons.cancel_outlined, size: 20),
+        ],
+      ), //const ,
+
+      onPressed: () => showDialog(
+        context: context, 
+        builder: (context) => confirmCancel(context, objList),
+        ),
+      // onPressed: () {
+      //   print('testing 1');
+      //   confirmCancel(context, objList);
+      // },
+    );
   } else {
+    //no cancel option when editing created jobs
     return const SizedBox.shrink();
   }
 }
--- lib/components/confirm_alert_dialog.dart
+++ lib/components/confirm_alert_dialog.dart
@@ -3,10 +3,10 @@ import 'package:hive/hive.dart';
 
 //! Alert pop up box to confirm Job deletion or page exit
 //arguments: build context, list of objects containing object to be deleted,
-//object to be deleted, object specific delete function 
+//object to be deleted, object specific delete function
 Widget confirmDelete(BuildContext context, List<HiveObject>? objList,
     HiveObject object, Function deletObject) {
-
+      
   //! Alert box
   return AlertDialog(
     title: const Text("Are you sure?", style: TextStyle(fontSize: 16)),
@@ -25,8 +25,41 @@ Widget confirmDelete(BuildContext context, List<HiveObject>? objList,
         TextButton(
           onPressed: () {
             Navigator.of(context).pop();
-            objList!.remove(object);      //remove object from object list
-            deletObject(object);          //delete object from Hive box
+            objList!.remove(object); //remove object from object list
+            deletObject(object); //delete object from Hive box
+          },
+          child: const Text('yes', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    ),
+  );
+}
+
+//! Alert pop up box to confirm cancel
+Widget confirmCancel(BuildContext context, List<HiveObject>? objList) {
+  print('testing 2');
+
+  //! Alert box
+  return AlertDialog(
+    title: const Text("Are you sure?", style: TextStyle(fontSize: 16)),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    ),
+    content: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //*no button
+        TextButton(
+          onPressed: Navigator.of(context).pop,
+          child: const Text('no', style: TextStyle(fontSize: 20)),
+        ),
+        //*yes button
+        TextButton(
+          onPressed: () {
+            for (var element in objList!) {
+              element.delete();
+            }
+            Navigator.of(context).pop();
           },
           child: const Text('yes', style: TextStyle(fontSize: 20)),
         ),
